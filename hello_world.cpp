2490b30c7cb1cdad32c18064630366fa36e3d03e
olgalobach <olga.lobach@inbox.ru>
2022-03-17 23:25:56 +0300

    change4

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@ int main()
 {
   std::string name;
   std::cout << "Enter your name: ";
-  // ввод имени
+  // ввод имени2
   std::cin >> name;
   std::cout << "Hello, " << name << "\n";
   return 0;
